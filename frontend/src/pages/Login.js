11d30159379c346c7eba30f14b245a2958bd38b8
Roni714 <rishabh.sareen.31@gmail.com>
2026-07-16 07:19:16 +0530

    Update login API URL for authentication

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `https://mern-app-api-bice.vercel.app/auth/login`;
+            const url = `https://mern-app-api-virid.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
