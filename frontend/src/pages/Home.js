e5920fb8c28da4e3a9fe39cb80ce91c7912fe3f2
Roni714 <rishabh.sareen.31@gmail.com>
2026-07-16 05:36:14 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            const url = "https://mern-app-api-mu.vercel.app/products";
+            const url = "https://mern-app-api-one.vercel.app/products";
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
