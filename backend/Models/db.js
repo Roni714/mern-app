362aa1282d7b6d543f974ed0f89214515e9b865d
Roni714 <rishabh.sareen.31@gmail.com>
2026-07-16 07:16:51 +0530

    Refactor MongoDB connection logic in db.js

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
+
 const mongo_url = process.env.MONGO_CONN;
 
-let isConnected = false;
-
-const connectDB = async () => {
-  if (isConnected) return;
-  await mongoose.connect(mongo_url);
-  isConnected = true;
-  console.log('MongoDB Connected...');
-};
-
-module.exports = connectDB;
+mongoose.connect(mongo_url)
+    .then(()=>{
+        console.log('MongoDB Connected...');
+    }).catch((err)=>{
+        console.log('MongoDB Connection Error: ', err);
+    });
